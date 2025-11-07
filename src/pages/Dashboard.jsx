5f84c65591256bc4b53276cdf39a0acbe58fff1d
Ghaida-Aloufi <ghaida2219@gmail.com>
2025-11-07 16:17:23 +0300

    Add button to add new job

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,12 @@ function Dashboard() {
   return (
     <>
       <NavBar />
+      <div className="relative ">
+        <div className="btn btn-ghost bg-blue-500 rounded-2xl flex  justify-around items-center absolute right-0 top-20 mr-20 w-40 h-15 ">
+          <span className="font-bold lg:text-2xl text-white">+</span>
+          <h1 className="text-white font-semibold ">Add New Job</h1>
+        </div>
+      </div>
     </>
   );
 }
