bf4599798bb104620765a5e7baf4e4ae85967fc9
Ghaida-Aloufi <ghaida2219@gmail.com>
2025-11-07 21:33:12 +0300

    Add form to dd a a new jo b post

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,9 +5,58 @@ function Dashboard() {
     <>
       <NavBar />
       <div className="flex flex-col m-30 h-[16vh] justify-between">
-        <div className="btn btn-ghost bg-blue-500 rounded-2xl flex  justify-around items-center  mr-20 w-40 h-15 ">
-          <span className="font-bold lg:text-2xl text-white">+</span>
-          <h1 className="text-white font-semibold ">Add New Job</h1>
+        <a
+          href="#my_modal_8"
+          className="btn w-45 rounded-xl p-6 bg-blue-500  text-white font-semibold "
+        >
+          <span className="font-bold text-md">+</span> Add New Job
+        </a>
+
+        <div className="modal" role="dialog" id="my_modal_8">
+          <div className="modal-box h-[70%] p-12 flex flex-col gap-3">
+            <h3 className="text-lg font-bold">Add New Job</h3>
+            <fieldset className="fieldset">
+              <label className="font-bold text-md">Title</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Type here"
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <label className="font-bold text-md">Company</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Type here"
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <label className="font-bold text-md">Location</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Type here"
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <label className="font-bold text-md">Salary</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Type here"
+              />
+            </fieldset>
+
+            <div className="modal-action">
+              <a href="#" className="btn">
+                Submit
+              </a>
+              <a href="#" className="btn">
+                Cancel
+              </a>
+            </div>
+          </div>
         </div>
 
         <label className="input w-full rounded-xl h-12 ">
@@ -27,12 +76,11 @@ function Dashboard() {
               <path d="m21 21-4.3-4.3"></path>
             </g>
           </svg>
-          <input type="search" required placeholder="Search by job title.." />
+          <input type="search" placeholder="Search by job title.." />
         </label>
       </div>
-      
-      <CardJob/>
-     
+
+      <CardJob />
     </>
   );
 }
