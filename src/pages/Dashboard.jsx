0ac163ea4311e92e8e9ccdc2d16a2ffc4c6b51d4
Ghaida-Aloufi <ghaida2219@gmail.com>
2025-11-07 17:21:27 +0300

    Add search input field

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,31 @@ function Dashboard() {
   return (
     <>
       <NavBar />
-      <div className="relative ">
-        <div className="btn btn-ghost bg-blue-500 rounded-2xl flex  justify-around items-center absolute right-0 top-20 mr-20 w-40 h-15 ">
+      <div className="flex flex-col m-30 h-[16vh] justify-between">
+        <div className="btn btn-ghost bg-blue-500 rounded-2xl flex  justify-around items-center  mr-20 w-40 h-15 ">
           <span className="font-bold lg:text-2xl text-white">+</span>
           <h1 className="text-white font-semibold ">Add New Job</h1>
         </div>
+
+        <label className="input w-full rounded-xl h-12 ">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" required placeholder="Search by job title.." />
+        </label>
       </div>
     </>
   );
